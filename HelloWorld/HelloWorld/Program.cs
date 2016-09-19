20a5b86e11273de53d7b88ef1f7af12f6f40b6a4
Afens <sheith.92@gmail.com>
2016-09-19 12:46:02 +0200

    HelloWorld

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String salir="hola";
+
+            while (!salir.Equals("si")|| !salir.Equals("Si") || !salir.Equals("SI") || !salir.Equals("sI"))
+            {
+                Console.WriteLine("Hola mundo");
+                Console.ReadLine();
+                Console.Beep(1000, 3000);
+                Console.WriteLine("Escribe true si quieres salir");
+                salir=Console.ReadLine();
+                Console.BackgroundColor = (ConsoleColor)15;
+            }
+        }
+    }
+}
